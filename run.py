ad71726f5aeb9c53d98e4d2090fd9413e503f7f4
snowman6-git <chldlfgks55@gmail.com>
2024-12-17 12:50:25 +0900

    노트 수정 리드미 수정

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ linked = Linked() #uid와 session의 인증과 추가 역할
 #===================================================================
 
 
-@app.get("/")
+@app.get("/") #메인페이지니까 가능하면 vue보내주는 방향으로 변경할것
 def main(request: Request):
     uid = request.session.get('uid')
     usession_id = request.session.get('usession')
@@ -31,25 +31,21 @@ def main(request: Request):
         }
     else: return "need login"
 
-    # if uid == None: return "You need login Agein" #세션을 보유하지만 서버에 인증이값이 없을경우
-    # if uLink.get(uid) == usession_id: return "perfect!" #세션을 보유하고, 인증값 마저 일치한다면
-    # elif uLink.get(uid) != usession_id: return "" #세션을 보유했으나, 인증값이 조작, 일치하지않는다면
-    # else: return "you need Login Agein!"
+@app.get("/verify") #세션 점검용
+def vertify(request: Request):
+    uid = request.session.get('uid')
+    usession_id = request.session.get('usession')
+    # print(uid, usession_id)
+    key_check = linked.is_real(uid, usession_id)
+    if key_check:
+        return {
+            "uid" : uid,
+            "usession" : usession_id,
+            "is_real?" : key_check,
+        }
+    else: return "need login"
 
-    # return {
-    #     "uid" : uid,
-    #     "usession" : request.session.get('usession'),
-    #     "uLink" : uLink,
-    #     "uLink vertify" : uLink.get(uid),
-    # }
-
-# @app.get("/ulink")
-# def main(request: Request):
-#     return {
-#         "uLink" : uLink,
-#     }
-
-@app.post("/verify")
+@app.post("/login") #나중에 이름 바꿀수 있으면 변경
 def vertify(request: Request, idpw: idpw):
     #실제로 디비랑 상호작용하는 코드, 개발단게에선 제외
     
